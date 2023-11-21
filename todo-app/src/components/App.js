e9bd5c4265a185c9fbaca69eee46e535e8c74adb
michele <mbarrant@iu.edu>
2023-11-21 15:20:48 -0500

    pushing todo-app

---
+++ todo-app/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from './todo/Todo';
+
+import '../style/app.css';
+
+const App = () => {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+};
+
+export default App;
